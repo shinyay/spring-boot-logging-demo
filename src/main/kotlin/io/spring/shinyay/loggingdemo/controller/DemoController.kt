07a13f7e2a4cea4d8d147e49566528e9df94f936
shinyay <shinya.com@gmail.com>
2022-08-08 15:32:43 +0900

    Add return message

--- src/main/kotlin/io/spring/shinyay/loggingdemo/controller/DemoController.kt
+++ src/main/kotlin/io/spring/shinyay/loggingdemo/controller/DemoController.kt
@@ -1,5 +1,6 @@
 package io.spring.shinyay.loggingdemo.controller
 
+import io.spring.shinyay.loggingdemo.logger
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 class DemoController {
 
     @GetMapping("/hello")
-    fun hello() {
-        
+    fun hello(): String {
+        return "Hello Spring Boot"
     }
 }
