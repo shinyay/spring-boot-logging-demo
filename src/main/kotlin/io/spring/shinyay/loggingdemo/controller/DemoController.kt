5e85124e830c900a36a4eb7ae2820435d1747e99
shinyay <shinya.com@gmail.com>
2022-08-08 15:29:37 +0900

    Add mapping path

--- src/main/kotlin/io/spring/shinyay/loggingdemo/controller/DemoController.kt
+++ src/main/kotlin/io/spring/shinyay/loggingdemo/controller/DemoController.kt
@@ -1,8 +1,15 @@
 package io.spring.shinyay.loggingdemo.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v1")
 class DemoController {
-    
+
+    @GetMapping("/hello")
+    fun hello() {
+        
+    }
 }
